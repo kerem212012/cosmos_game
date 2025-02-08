4ba9c9fe0ecfefe46c73cd90a2851103d43db68f
kerem212012 <rockstarfoxykerem@gmail.com>
2025-02-08 18:53:49 +0300

    stars blink in diferent time

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ TIC_TIMEOUT = 0.1
 async def blink(canvas, row, column, symbol='*'):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        for _ in range(20):
+        for _ in range(random.randint(0,20)):
             await asyncio.sleep(0)
         canvas.addstr(row, column, symbol)
-        for _ in range(3):
+        for _ in range(random.randint(0,3)):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
-        for _ in range(5):
+        for _ in range(random.randint(0,5)):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        for _ in range(3):
+        for _ in range(random.randint(0,3)):
             await asyncio.sleep(0)
 
 
